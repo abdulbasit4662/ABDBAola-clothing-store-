c26381aec49de1baa4365eaab451d30a195ecd24
abdulbasit4662 <olalekanabdulbasit49@gmail.com>
2026-02-17 15:29:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,340 @@
+let products = [
+    
+ ["https://iili.io/q9QcS2I.md.webp",
+"https://iili.io/q9QcvrN.md.webp",
+"https://iili.io/q9Qck1p.md.webp",
+ "https://iili.io/q9QceBR.md.webp",
+"https://iili.io/q9Qcrpn.md.webp"],
+    
+    
+["https://iili.io/q9t6fTP.md.webp",      
+ "https://iili.io/q9t63vV.md.webp" ,
+ "https://iili.io/q9t6FyB.md.webp"],
+    
+    
+  ["https://iili.io/q9D6FON.md.webp",
+ "https://iili.io/q9D63Rp.md.webp",
+ "https://iili.io/q9D6KbI.md.webp",
+ "https://iili.io/q9D6JUv.md.webp",
+ "https://iili.io/q9D6x0G.md.webp"], 
+    
+    
+  ["https://iili.io/q9b0X0G.md.webp",
+ "https://iili.io/q9b0Wfs.md.webp",
+ "https://iili.io/q9b0hgf.md.webp"],
+    
+    
+["https://iili.io/q9mYF5B.md.jpg", 
+ "https://iili.io/q9mY3JV.md.jpg"],  
+    
+    
+["https://iili.io/q9pglcJ.md.jpg",
+ "https://iili.io/q9pgY6g.md.jpg"],    
+   
+    
+  ["https://iili.io/q9yagcJ.jpg",
+ "https://iili.io/q9yavMF.jpg",
+ "https://iili.io/q9yaUFa.jpg"],  
+    
+  ["https://iili.io/q9yQUfp.jpg",
+"https://iili.io/q9yQP5X.jpg",
+ "https://iili.io/q9yQ8sR.jpg",
+"https://iili.io/q9yQrgI.jpg",
+"https://iili.io/q9yQQzG.jpg",
+ "https://iili.io/q9yQZXf.jpg"], 
+    
+    
+ ["https://iili.io/qH9IwYB.jpg",
+"https://iili.io/qH9INkP.jpg",
+"https://iili.io/qH9IXrQ.jpg"],      
+    
+    
+["https://iili.io/qH9Las4.jpg",
+ "https://iili.io/qH9L012.jpg",
+"https://iili.io/qH9L7zG.jpg"],        
+    
+    
+ ["https://iili.io/qHHkdkQ.jpg",
+ "https://iili.io/qHHemBe.jpg",
+ "https://iili.io/qHHeyrb.jpg"],        
+    
+    
+ ["https://iili.io/qHJqEU7.jpg",
+ "https://iili.io/qHJqMJ9.jpg",
+ "https://iili.io/qHJqcil.jpg",
+ "https://iili.io/qHJqVRe.png"],       
+    
+    
+ ["https://iili.io/qHJhsgR.jpg",
+ "https://iili.io/qHJhPqJ.jpg",
+"https://iili.io/qHJhrWg.jpg",
+ "https://iili.io/qHJh4sa.jpg"],       
+    
+    
+  ["https://iili.io/qHJDg8Q.jpg",
+"https://iili.io/qHJDUcx.jpg",
+ "https://iili.io/qHJDv6b.jpg"],  
+    
+    
+["https://iili.io/qHdxkmP.png",
+ "https://iili.io/qHdxxeI.jpg",
+ "https://iili.io/qHdxFsa.jpg",
+ "https://iili.io/qHdxfqJ.jpg"],      
+    
+    
+["https://iili.io/qHdlxku.png",
+ "https://iili.io/qHdln29.jpg",
+"https://iili.io/qHdlTIj.png",
+"https://iili.io/qHdlo7e.jpg"],        
+    
+    
+ ["https://iili.io/qHdNrn1.png",
+"https://iili.io/qHdNN3b.png",
+ "https://iili.io/qHdNhGe.jpg",
+"https://iili.io/qHdNXn9.jpg"],          
+    
+    
+ ["https://iili.io/qHd4NWv.jpg",
+ "https://iili.io/qHd4wzJ.jpg",
+ "https://iili.io/qHd4hba.jpg"],       
+    
+    
+  ["https://iili.io/qHdbYYv.webp",
+ "https://iili.io/qHdb73J.webp",
+"https://iili.io/qHdbR4a.jpg"], 
+    
+    
+ ["https://iili.io/qH2xDx9.webp",
+ "https://iili.io/qH2xyKb.webp",
+"https://iili.io/qH2xmiu.jpg"],        
+    
+    
+ ["https://iili.io/qH2jpu2.webp",
+ "https://iili.io/qH2w9Z7.jpg",
+"https://iili.io/qH2jbyl.jpg",
+"https://iili.io/qH2jDv4.webp",
+"https://iili.io/qH2wJn9.webp",
+ "https://iili.io/qH2wdGe.webp"],   
+    
+    
+ ["https://iili.io/qH3n4ne.webp",
+"https://iili.io/qH3nUw7.webp",
+"https://iili.io/qH3ngt9.webp",
+ "https://iili.io/qH3nSuS.webp"],      
+    
+    
+ ["https://iili.io/qH3nSuS.webp",
+"https://iili.io/qH3RNkb.webp",
+ "https://iili.io/qH3ROpj.webp",
+ "https://iili.io/qH2xmiu.jpg"],      
+    
+    
+  ["https://iili.io/qH307oJ.webp",
+ "https://iili.io/qH300cN.webp",
+"https://iili.io/qH30lFp.webp",
+"https://iili.io/qH30Awg.webp",
+ "https://iili.io/qH301SI.webp"],     
+    
+    
+  ["https://iili.io/qH3hB3J.jpg",
+"https://iili.io/qH3hCYv.jpg",
+"https://iili.io/qH3hf4a.jpg",
+"https://iili.io/qH3hKEg.jpg",
+"https://iili.io/qH3hnvR.jpg"],        
+    
+    
+ ["https://iili.io/qH3hKEg.jpg",
+"https://iili.io/qH3gXY7.jpg",
+"https://iili.io/qH3gGEl.jpg",
+ "https://iili.io/qH3gM42.jpg",
+ "https://iili.io/qH3hnvR.jpg",
+ "https://iili.io/qH3gjpe.jpg"],       
+    
+    
+  ["https://iili.io/qH3mAoN.webp",
+ "https://iili.io/qH3mTtp.webp",
+ "https://iili.io/qH3mRVI.webp",
+"https://iili.io/qH3m5Pt.webp"],       
+    
+    
+["https://iili.io/qHFuZkG.webp",
+"https://iili.io/qHFuP1t.webp",
+ "https://iili.io/qHFubI4.jpg",
+"https://iili.io/qHFutpf.jpg",
+"https://iili.io/qHFupQ2.jpg"],        
+   
+    
+["https://iili.io/qHFjQaa.jpg",
+ "https://iili.io/qHFjL3g.jpg",
+ "https://iili.io/qHFjPG1.jpg",
+ "https://iili.io/qHFji6F.webp"],     
+    
+    
+ ["https://iili.io/qHFEaJ2.jpg",
+ "https://iili.io/qHFElO7.jpg",
+ "https://iili.io/qHFPSQn.jpg",
+ "https://iili.io/qHFE0b9.jpg",
+"https://iili.io/qHFPgBs.jpg"],       
+    
+    
+ ["https://iili.io/qHn8N0G.jpg",
+"https://iili.io/qHn8v5l.jpg",
+"https://iili.io/qHn8Ogf.jpg"],
+    
+    
+ ["https://iili.io/qHnmXyP.webp",
+"https://iili.io/qHnmW8B.webp",
+ "https://iili.io/qHnmVaV.jpg"],       
+    
+    
+["https://iili.io/qHWDkf1.webp",
+"https://iili.io/qHWDOiP.webp",
+"https://iili.io/qHWDNWB.webp",
+"https://iili.io/qHWDwxV.webp"],       
+    
+    
+["https://iili.io/qHXEK6G.webp",
+ "https://iili.io/qHXEFGs.webp",
+ "https://iili.io/qHXEdZX.webp",
+ "https://iili.io/qHXE3nn.webp"],        
+    
+    
+ ["https://iili.io/qHhy8EQ.jpg",
+ "https://iili.io/qHhyg3B.jpg",
+ "https://iili.io/qHhyS4V.jpg",
+"https://iili.io/qHhyvCx.jpg"],       
+   
+    
+["https://iili.io/qHjlDiu.jpg",
+ "https://iili.io/qHjlZx9.jpg",
+"https://iili.io/qHjlsNS.webp",
+"https://iili.io/qHjlmKb.jpg"],       
+    
+    
+ ["https://iili.io/qHjQQae.webp",
+"https://iili.io/qHjQLF9.webp",
+ "https://iili.io/qHjQtyb.jpg",
+ "https://iili.io/qHjQZ8u.jpg"],     
+    
+    
+  ["https://iili.io/qHwfziX.jpg",
+"https://iili.io/qHwfTfn.webp",
+"https://iili.io/qHwfxVt.jpg",
+"https://iili.io/qHwfuls.webp"],       
+    
+    
+ ["https://iili.io/qHw0tN2.jpg",
+"https://iili.io/qHw0Q94.jpg",
+"https://iili.io/qHw0DtS.webp"],       
+    
+    
+["https://iili.io/qHPSr4j.jpg", 
+"https://iili.io/qHPSUCu.jpg",
+"https://iili.io/qHPSPYQ.jpg",
+"https://iili.io/qHPSivV.jpg"],
+    
+     
+["https://iili.io/qHiJEBa.webp",
+"https://iili.io/qHiJ0Lg.webp"],        
+    
+    
+ ["https://iili.io/qHiTfY7.jpg",
+ "https://iili.io/qHiTqv9.jpg"],       
+    
+    
+["https://iili.io/qHiXVvn.jpg",
+"https://iili.io/qHiXwQ4.jpg",
+"https://iili.io/qHiXG2t.jpg"],       
+    
+    
+["https://iili.io/qHisYwx.jpg",
+ "https://iili.io/qHisaZQ.jpg"],       
+    
+    
+["https://iili.io/qHs35Hx.jpg",
+"https://iili.io/qHs3ucb.jpg",
+ "https://iili.io/qHs3ASj.jpg"],       
+    
+     
+["https://iili.io/qHsXJcP.webp",
+ "https://iili.io/qHsX3HF.jpg",
+"https://iili.io/qHiJ0Lg.webp",
+"https://iili.io/qHsXdS1.jpg"],       
+    
+    
+["https://iili.io/qHsgQ1e.jpg",
+ "https://iili.io/qHsgLB9.jpg",
+ "https://iili.io/qHsgPXS.jpg"],      
+    
+    
+["https://iili.io/qJ95bR9.jpg",
+ "https://iili.io/qJ979ob.jpg",
+ "https://iili.io/qJ95pDu.jpg"],       
+    
+     
+ ["https://iili.io/qJ9iV5v.jpg",
+ "https://iili.io/qJ9iWeR.jpg"],
+    
+     
+ ["https://iili.io/qJIz9St.webp",
+"https://iili.io/qJIxylI.webp"],       
+    
+    
+["https://iili.io/qJIlHwN.jpg",
+"https://iili.io/qJIlJtI.jpg",
+"https://iili.io/qJIl9up.jpg"],       
+    
+    
+ ["https://iili.io/qJIwDKl.jpg",
+ "https://iili.io/qJIl9up.jpg",
+"https://iili.io/qJIwmSS.jpg"],       
+    
+    
+ ["https://iili.io/qJIiiMB.jpg",
+"https://iili.io/qJIiQF1.jpg",
+"https://iili.io/qJIis6P.jpg"],       
+    
+     
+ ["https://iili.io/qJTFJmQ.jpg",
+"https://iili.io/qJTFHkx.jpg",
+"https://iili.io/qJTF97j.jpg",
+"https://iili.io/qJT3ydb.jpg",
+"https://iili.io/qJTF2IV.webp"],       
+    
+    
+["https://iili.io/qJT7WRp.jpg",
+ "https://iili.io/qJT7VJR.jpg",
+"https://iili.io/qJT7E0J.jpg"],        
+    
+    
+["https://iili.io/qJTheun.jpg",
+ "https://iili.io/qJThNyX.jpg"],      
+    
+    
+    ];
+
+let currentIndex = new Array(products.length).fill(0);
+
+function nextImage(productNumber){
+    currentIndex[productNumber]++;
+
+    if(currentIndex[productNumber] >= products[productNumber].length){
+        currentIndex[productNumber]=0;
+    }
+
+    document.getElementById("img"+productNumber).src =
+        products[productNumber][currentIndex[productNumber]];
+}
+
+function searchProducts(){
+    let input=document.getElementById("search").value.toLowerCase();
+    let items=document.getElementsByClassName("product");
+
+    for(let i=0;i<items.length;i++){
+        let name=items[i]
+            .getElementsByClassName("product-name")[0]
+            .innerText.toLowerCase();
+
+        items[i].style.display=name.includes(input)?"":"none";
+    }
+}
